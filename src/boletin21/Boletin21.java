0001525b0ceb07f69bf60629ce9b7ea497ae7c79
antonio <antonio@biruseto>
2017-03-22 12:32:19 +0100

    boletin 21  main

---
+++ src/boletin21/Boletin21.java
@@ -0,0 +1,14 @@
+
+package boletin21;
+
+
+public class Boletin21 {
+
+   
+    public static void main(String[] args) {
+         Leer le = new Leer();
+        le.mostrarLista("boletin21.txt");
+        le.añadir("boletin21.txt");
+    }
+    
+}
